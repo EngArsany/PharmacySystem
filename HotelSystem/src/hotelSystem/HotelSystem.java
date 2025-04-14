6cad7572677b1198199fabdba6b5891a7363418a
Andrew <andrewehab417@gmail.com>
2025-04-15 01:43:54 +0200

    using netbeans

--- HotelSystem/src/hotelSystem/HotelSystem.java
+++ HotelSystem/src/hotelSystem/HotelSystem.java
@@ -2,10 +2,11 @@ package hotelSystem;
 
 public class HotelSystem {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is Andrew");
-		System.out.println("Hello");
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("This is Andrew");
+        System.out.println("This netbeans");
+        System.out.println("Hello");
+    }
 
 }

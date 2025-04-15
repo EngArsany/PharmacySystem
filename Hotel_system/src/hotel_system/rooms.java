c2d618ebe6986498051e55ccb536dbaed6fdf5de
Andrew <andrewehab417@gmail.com>
2025-04-15 02:08:37 +0200

    made a new class to see the difference (test)

---
+++ Hotel_system/src/hotel_system/rooms.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel_system;
+
+/**
+ *
+ * @author andre
+ */
+public class rooms {
+    // trying making new class
+}
